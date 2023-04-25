edfc621f047484105ca3ccfeb552ead8e6e38313
Inácio Raimundo <84747204+inacio000@users.noreply.github.com>
2023-04-26 02:14:23 +0300

    finish the challenge

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,35 @@
-export function SideBar() {
+import { Button } from '../components/Button';
+import '../styles/sidebar.scss';
+
+interface Tprops {
+  genres: any;
+  genreId: any;
+  selected: any;
+}
+
+export function SideBar(props: Tprops) {
   // Complete aqui
+
+  function handleClickButton(id: number) {
+    props.selected(id);
+  }
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {props.genres.map((genre: any) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={props.genreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
